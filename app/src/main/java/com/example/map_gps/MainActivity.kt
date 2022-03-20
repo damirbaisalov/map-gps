8971f6115d08f421eef3e4076c3fa3843dabe0c4
damirbaisalov <baisalovdamir@gmail.com>
2022-03-20 16:19:07 +0600

    bottom-nav v1

--- app/src/main/java/com/example/map_gps/MainActivity.kt
+++ app/src/main/java/com/example/map_gps/MainActivity.kt
@@ -3,9 +3,28 @@ package com.example.map_gps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.firstFragment,
+//            R.id.secondFragment,
+//            R.id.thirdFragment
+//        ))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
